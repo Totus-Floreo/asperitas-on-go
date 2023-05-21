83242a870dd8b1e6db926143ca00e7d35426830c
Timur Kulakov <kulakovcore@gmail.com>
2023-05-22 02:33:38 +0300

    stateful token and implementing repository pattern

--- pkg/model/token.go
+++ pkg/model/token.go
@@ -1,5 +1,13 @@
 package model
 
+import "context"
+
+type ITokenStorage interface {
+	GetToken(context.Context, string) (string, error)
+	CreateToken(context.Context, string, string) error
+	DeleteToken(context.Context, string) error
+}
+
 type IJWTService interface {
 	GenerateToken(*User) (string, error)
 	VerifyToken(string) (*Author, error)
