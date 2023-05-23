9decc0cc24e0240f8188ef1fee518c939101b251
Timur Kulakov <kulakovcore@gmail.com>
2023-05-24 02:27:39 +0300

    New user_repository based on PostgreSQL.

--- pkg/repository/pgx/user_repository.go
+++ pkg/repository/pgx/user_repository.go
@@ -1,9 +1,12 @@
-package repository
+package pgx
 
 import (
 	"context"
+	"log"
 
 	"github.com/Totus-Floreo/asperitas-on-go/pkg/model"
+	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5"
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
@@ -17,7 +20,7 @@ func NewUserStorage(connPool *pgxpool.Pool) *UserStorage {
 	}
 }
 
-func (s *UserStorage) GetUser(ctx context.Context, userID string) (*model.User, error) {
+func (s *UserStorage) GetUser(ctx context.Context, username string) (*model.User, error) {
 	conn, err := s.connPool.Acquire(ctx)
 	if err != nil {
 		return nil, err
@@ -30,12 +33,47 @@ func (s *UserStorage) GetUser(ctx context.Context, userID string) (*model.User, 
 	}
 	defer tx.Rollback(ctx)
 
-	// err := tx.QueryRow(ctx, "SELECT * FROM users WHERE users.")
+	user := &model.User{}
+	if err := tx.QueryRow(ctx, "SELECT * FROM users WHERE username = $1", username).Scan(&user.ID, &user.Username, &user.Password); err != nil {
+		if err == pgx.ErrNoRows {
+			return nil, model.ErrUserNotFound
+		} else {
+			return nil, err
+		}
+	}
 
 	err = tx.Commit(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	return &model.User{}, nil
+	return user, nil
+}
+
+func (s *UserStorage) AddUser(ctx context.Context, user *model.User) error {
+	conn, err := s.connPool.Acquire(ctx)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	defer conn.Release()
+
+	tx, err := conn.Begin(ctx)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback(ctx)
+
+	user.ID = uuid.New().String()
+	_, err = tx.Exec(ctx, "INSERT INTO users(id, username, password) VALUES ($1, $2, $3)", user.ID, user.Username, user.Password)
+	if err != nil {
+		return err
+	}
+
+	err = tx.Commit(ctx)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
